d8e136952e21b66214851e166acd09cb6832a32f
ArshadRahim <arshadraheem000@gmail.com>
2022-01-12 14:11:11 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  Hey there
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+    head() {
+      return {
+        title: 'Welcome To Dad Jokes'
+      }
+      },
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+}
+</script>
